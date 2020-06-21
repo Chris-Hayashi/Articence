6bb14e241c14410d797d2d39f0526707d2ca9b28
Christopher Hayashi <Chrisrhayashi@gmail.com>
2020-06-21 11:50:53 -0700

    Created react app and added components (Navbar and Google Login)

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar />
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App;
---
+++ client/src/components/GoogleLogin/index.js
@@ -0,0 +1,11 @@
+import React, { Component} from 'react';
+
+class GoogleLogin extends Component {
+    render() {
+        return (
+            <div>Google Login</div>
+        );
+    }
+}
+
+export default GoogleLogin;
