1a080d3ca16d8b0475b6727962ff9947d6cd7836
Christopher Hayashi <Chrisrhayashi@gmail.com>
2020-06-22 11:11:01 -0700

    added TableSort component

---
+++ client/src/components/TableSort/index.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react';
+
+class TableSort extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default TableSort;
